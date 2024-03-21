2aa3c4e28170692255d177380887de8013f0aed9
Daniel Gil <dannyjaygil@gmail.com>
2024-03-21 14:05:03 +0000

    Build API to update and delete leads

---
+++ app/api/leads/[id]/route.ts
@@ -0,0 +1,78 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return NextResponse.error();
+
+  const body = await request.json();
+
+  const {
+    name,
+    number,
+    email,
+    date,
+    nationality,
+    type,
+    budget,
+    area,
+    beds,
+    property,
+    extra,
+    comment,
+  } = body;
+
+  const lead = await prisma.lead.findUnique({
+    where: { id: params?.id },
+  });
+
+  if (!lead)
+    return NextResponse.json({ error: "Invalid lead" }, { status: 404 });
+
+  const updatedLead = await prisma.lead.update({
+    where: { id: lead.id },
+    data: {
+      name,
+      number,
+      email,
+      date,
+      nationality,
+      type,
+      budget,
+      area,
+      beds,
+      property,
+      extra,
+      comment,
+    },
+  });
+
+  return NextResponse.json(updatedLead);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return NextResponse.error();
+
+  const lead = await prisma.lead.findUnique({
+    where: { id: params.id },
+  });
+
+  if (!lead)
+    return NextResponse.json({ error: "Invalid lead" }, { status: 404 });
+
+  await prisma.lead.delete({
+    where: { id: lead.id },
+  });
+
+  return NextResponse.json({});
+}
