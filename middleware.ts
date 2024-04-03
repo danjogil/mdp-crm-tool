3cb6ab1d416969fee421e7f1360500304508b49c
Daniel Gil <dannyjaygil@gmail.com>
2024-04-03 10:05:18 +0300

    Update middleware routes

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,13 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard", "/leads", "/leads/new", "/leads/:id+"],
+  matcher: [
+    "/dashboard",
+    "/leads",
+    "/leads/new",
+    "/leads/:id+",
+    "/properties",
+    "/properties/new",
+    "/properties/:id+",
+  ],
 };
